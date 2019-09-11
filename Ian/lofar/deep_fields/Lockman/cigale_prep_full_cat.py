a598659225510ddb6d39a167b534e28e6de2775e
Ian McCheyne <imccheyne@lofar-server.data>
2019-09-11 15:59:20 +0100

    modified cigale prep so it can be run on herts cluster

--- Ian/lofar/deep_fields/Lockman/cigale_prep_full_cat.py
+++ Ian/lofar/deep_fields/Lockman/cigale_prep_full_cat.py
@@ -29,7 +29,7 @@ def pop_list(ls,indices):
 
 
 #photz = Table.read('data/photz/Bootes_opt_spitzer_merged_vac_opt3as_irac4as_all_hpx_public.fits')
-photz = Table.read('/beegfs/lofar/deepfields/science_ready_catalogs/Bootes_opt_spitzer_merged_vac_opt3as_irac4as_all_hpx_public.fits')
+photz = Table.read('/beegfs/lofar/deepfields/science_ready_catalogs/LH_opt_spitzer_merged_vac_opt3as_irac4as_all_hpx_public.fits')
 
 # In[4]:
 
@@ -44,7 +44,7 @@ cigale_input = photz.copy()
 
 
 #filters_file = open('data/photz/filters/filter.bootes_mbrown_2014a.res.info','r')
-filters_file = open('/beegfs/lofar/deepfields/science_ready_catalogs/filter_information/filter.bootes_mbrown_2014a.res.info','r')
+filters_file = open('/beegfs/lofar/deepfields/science_ready_catalogs/filter_information/Lockman-SWIRE_filters.res.info','r')
 filters = []
 for line in filters_file:
     temp = line.split(' ')
@@ -64,7 +64,7 @@ for n,line in enumerate(filters):
 
 
 #translate_file = open('data/photz/filters/brown.zphot.2014.translate','r')
-translate_file = open('/beegfs/lofar/deepfields/science_ready_catalogs/filter_information/brown.zphot.2014.translate','r')
+translate_file = open('/beegfs/lofar/deepfields/science_ready_catalogs/filter_information/LH.filter.translate','r')
 translate = []
 for line in translate_file:
     if '#' in line:
